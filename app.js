2ad022fdb6355ea8ca4df39f7b3011842b7b4db2
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-11-15 23:33:53 -0800

    project done

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ require('dotenv').config();
 if(!process.argv[2]){
     console.log('Please provide an address')
 } else{
-    geoCode(process.argv[2], (err, data) => {
+    geoCode(process.argv[2], (err, { latitude, longitude, location }) => {
         if(err){
             return console.log(err)
         }
     
-        forecast(data.latitude, data.longitude, (err, forecastData) => {
+        forecast(latitude, longitude, (err, forecastData) => {
             if(err){
                 console.log(err)
             }
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData)
         })
     });
